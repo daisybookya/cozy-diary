40a06f0f6eef20b34176f1748ae8048999497b0e
daisy <daisybookya@gmail.com>
2024-02-17 15:07:02 +0800

    fix basename2

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/cozy-diary">
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/cozy-diary/"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
